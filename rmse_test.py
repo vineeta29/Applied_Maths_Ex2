df2190abc7ba4016e576762b4486698a193b2ecc
vineeta29 <156238544+vineeta29@users.noreply.github.com>
2024-05-22 17:50:47 -0400

    RMSE

---
+++ rmse_test.py
@@ -0,0 +1,13 @@
+import rmse
+
+def test_rmse():
+    predictions = [1, 2, 3, 4, 5]
+    targets = [1, 2, 3, 4, 5]
+
+    assert rmse.rmse(predictions, targets) == 0
+    print("test_addition: 5 points")
+    predictions = [2, 3, 4, 5, 6]
+    targets = [1, 2, 3, 4, 5]
+
+    assert rmse.rmse(predictions, targets) == 1
+    print("test_addition: 1 points")
